1dad8cf30f35a1af39f85ef30a01e2a1055ea2ca
晓旸 <zqzhuang888@qq.com>
2019-10-10 18:56:33 +0800

    fix: rename factory to interceptor (#5)
    * feat: init
    
    * feat: init
    
    * fix: rename module
    
    * feat: add mini app api factory
    
    * chore: update README
    
    * chore: update README
    
    * feat: refact callback name
    
    * fix: rename factory to interceptor
    
    * fix: rename factory to interceptor
    
    * fix: rename factory to interceptor
    
    * feat: add ci
    
    * feat: add ci
    
    * fix: rename factory to interceptor

---
+++ packages/miniapp-api-interceptor/src/index.ts
@@ -0,0 +1,66 @@
+export interface ICallback {
+  (res: any): void;
+};
+
+export interface IOptions {
+  success?: ICallback;
+  fail?: ICallback;
+  complete?: ICallback;
+};
+
+export interface ICallbacks {
+  success?: ICallback;
+  fail?: ICallback;
+  complete?: ICallback;
+};
+
+export interface IEventCallback {
+  (res: any): void;
+};
+
+export interface IProcess {
+  method: string;
+  optionsInterceptor?: (options: IOptions) => any;
+  successInterceptor?: (res: any) => any;
+  failInterceptor?: (err: any) => any;
+};
+
+export const listenerInterceptor = (platformApi: any, process: IProcess) => {
+  return (callback: IEventCallback) => {
+    platformApi[process.method](callback);
+  };
+};
+
+export const apiInterceptor = (platformApi: any, process: IProcess) => {
+  return (options: IOptions = {}) => {
+    if (process.optionsInterceptor) {
+      options = process.optionsInterceptor(options);
+    }
+    return new Promise((resolve, reject): void => {
+      const callbacks: ICallbacks = {};
+      ['success', 'fail', 'complete'].forEach(k => {
+        callbacks[k] = options[k];
+      });
+      Object.assign(options, {
+        success: (res: any) => {
+          if (process.successInterceptor) {
+            res = process.successInterceptor(res);
+          }
+          callbacks.success && callbacks.success(res);
+          resolve(res);
+        },
+        fail: (err: any): void => {
+          if (process.failInterceptor) {
+            err = process.failInterceptor(err);
+          }
+          callbacks.fail && callbacks.fail(err);
+          reject(err);
+        },
+        complete: (res: any): void => {
+          callbacks.complete && callbacks.complete(res);
+        }
+      });
+      platformApi[process.method](options);
+    });
+  };
+};
