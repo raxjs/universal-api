f27a9072bcdd423573a31f311aaed05da742c048
balloonzzq <zqzhuang888@qq.com>
2019-10-31 15:25:11 +0800

    fix: optimized export mode

--- packages/clipboard/src/index.ts
+++ packages/clipboard/src/index.ts
@@ -1,13 +1,26 @@
-import { isWeex, isMiniApp } from 'universal-env';
+import { isWeb, isWeex, isMiniApp, isWeChatMiniprogram } from 'universal-env';
+import webModule from './web';
+import weexModule from './weex';
+import miniAppModule from './miniapp/ali';
+import weChatModule from './miniapp/wechat';
+import { Clipboard } from './types';
 
-let clipboard: any;
+let Clipboard: Clipboard;
 
-if (isWeex) {
-  clipboard = require('./weex').default;
-} else if (isMiniApp) {
-  clipboard = require('./miniapp').default;
-} else {
-  clipboard = require('./web').default;
+if (isWeb) {
+  Clipboard = webModule;
 }
 
-export default clipboard;
+if (isWeex) {
+  Clipboard = weexModule;
+}
+
+if (isMiniApp) {
+  Clipboard = miniAppModule;
+}
+
+if (isWeChatMiniprogram) {
+  Clipboard = weChatModule;
+}
+
+export default Clipboard;
