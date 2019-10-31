2f7966bff4ec4937f65329480bafd9f048ef0614
晓旸 <zqzhuang888@qq.com>
2019-10-31 18:31:15 +0800

    fix: optimized export mode (#13)

--- packages/navigate/src/types.ts
+++ packages/navigate/src/types.ts
@@ -11,3 +11,9 @@ export interface GoOptions {
   step: number;
   animated?: boolean;
 }
+
+export interface Navigate {
+  push(options: PushOptions): Promise<null>;
+  go(options: GoOptions): Promise<null>;
+  pop(options: GoOptions): Promise<null>;
+}
