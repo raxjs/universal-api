4eb692cfc8bfe9b45470a0c11b5ca5019eb47511
fushen <fushen.jzw@alibaba-inc.com>
2019-11-26 17:02:03 +0800

    chore: add check getapp in miniapp

--- packages/env/src/index.ts
+++ packages/env/src/index.ts
@@ -2,6 +2,7 @@ declare const callNative: any;
 declare const WXEnvironment: any;
 declare const my: any;
 declare const wx: any;
+declare const getApp: any;
 
 function isUndef(type): boolean {
   return type === 'undefined';
@@ -13,7 +14,6 @@ export const isNode =
 export const isWeex =
   !isUndef(typeof callNative) ||
   !isUndef(typeof WXEnvironment) && WXEnvironment.platform !== 'Web';
-export const isMiniApp = !isUndef(typeof my) && !isUndef(my.navigateTo);
+export const isMiniApp = !isUndef(typeof getApp) && !isUndef(typeof my) && !isUndef(my.navigateTo);
 export const isWeChatMiniProgram =
-  !isUndef(typeof wx) && !isUndef(wx.navigateTo);
-export const isWeChatMiniprogram = isWeChatMiniProgram;
+!isUndef(typeof getApp) && !isUndef(typeof wx) && !isUndef(wx.navigateTo);
