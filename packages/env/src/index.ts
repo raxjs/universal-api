cc9037f52104fe6c69863736d7dea940621264aa
fushen <fushen.jzw@alibaba-inc.com>
2019-11-27 12:40:33 +0800

    feat: enhance env check

--- packages/env/src/index.ts
+++ packages/env/src/index.ts
@@ -2,7 +2,6 @@ declare const callNative: any;
 declare const WXEnvironment: any;
 declare const my: any;
 declare const wx: any;
-declare const getApp: any;
 
 function isUndef(type): boolean {
   return type === 'undefined';
@@ -14,6 +13,5 @@ export const isNode =
 export const isWeex =
   !isUndef(typeof callNative) ||
   !isUndef(typeof WXEnvironment) && WXEnvironment.platform !== 'Web';
-export const isMiniApp = !isUndef(typeof getApp) && !isUndef(typeof my) && !isUndef(my.navigateTo);
-export const isWeChatMiniProgram =
-!isUndef(typeof getApp) && !isUndef(typeof wx) && !isUndef(wx.navigateTo);
+export const isMiniApp = !isUndef(typeof my) && my !== null && !isUndef(my.alert);
+export const isWeChatMiniProgram = !isUndef(typeof wx) && wx !== null && (!isUndef(wx.login) || !isUndef(wx.miniProgram));
