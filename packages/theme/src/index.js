39582814c6ac9e2ada8400137adbf06cce68ce09
yacheng.sz <yacheng.sz@taobao.com>
2020-01-06 23:47:33 +0800

    feat: Using CSS custom properties (variables)

--- packages/theme/src/index.js
+++ packages/theme/src/index.js
@@ -1,18 +1,57 @@
 import raxUICore from '@rax-ui/core';
 
-let store = {
-  theme: raxUICore,
-};
+const PREFIX = '--';
+const bodyStyle = document.body && document.body.style;
+const isThemeWeb = typeof window === 'object' && typeof window.getComputedStyle === 'function' && typeof bodyStyle.setProperty === 'function';
 
-export function setTheme(config) {
-  store = {
-    theme: {
-      ...store.theme,
-      ...config,
-    }
-  };
+// use the base theme
+let pageTheme = { ...raxUICore };
+
+// use the window.getComputedStyle 
+if (isThemeWeb) {
+	// Using CSS custom properties (variables)
+	// Create CSS variables on body
+	for (let key in pageTheme) {
+		bodyStyle.setProperty(PREFIX + key, pageTheme[key]);
+	}
+} else if (typeof window === 'object') {
+	// init getComputedStyle for weex or too old browser
+	window.getComputedStyle = () => {
+		return {
+			getPropertyValue: (key) => {
+				console.log('getPropertyValue', key);
+				return pageTheme[key.substr(2)]
+			}
+		}
+	};
 }
 
-export function getCSSVariables() {
-  return store.theme;
+// set new CSS variables
+export function setCSSVariables(config) {
+	if (isThemeWeb) {
+		for (let key in config) {
+			bodyStyle.setProperty(PREFIX + key, config[key]);
+		}
+	} else if (typeof window === 'object') {
+		pageTheme = {
+	    ...pageTheme,
+	    ...config,
+		};
+	}
+}
+
+// set new CSS variable
+export function setCSSVariable(key, value) {
+	if (isThemeWeb) {
+		bodyStyle.setProperty(PREFIX + key, value);
+	} else {
+		pageTheme[key] = value;
+	}
+}
+
+// get CSS variable
+export function getCSSVariable(key) {
+	console.log('getCSSVariable', key);
+	theme = window.getComputedStyle(typeof document == 'object' ? document.body : null);
+	return theme.getPropertyValue(PREFIX + key);
 }
