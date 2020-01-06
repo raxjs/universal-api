ee2b256a2890420d79301660108bfaa255e0779a
yacheng.sz <yacheng.sz@taobao.com>
2020-01-06 23:52:21 +0800

    fix: lint

--- packages/theme/src/index.js
+++ packages/theme/src/index.js
@@ -9,49 +9,48 @@ let pageTheme = { ...raxUICore };
 
 // use the window.getComputedStyle 
 if (isThemeWeb) {
-	// Using CSS custom properties (variables)
-	// Create CSS variables on body
-	for (let key in pageTheme) {
-		bodyStyle.setProperty(PREFIX + key, pageTheme[key]);
-	}
+  // Using CSS custom properties (variables)
+  // Create CSS variables on body
+  for (let key in pageTheme) {
+    bodyStyle.setProperty(PREFIX + key, pageTheme[key]);
+  }
 } else if (typeof window === 'object') {
-	// init getComputedStyle for weex or too old browser
-	window.getComputedStyle = () => {
-		return {
-			getPropertyValue: (key) => {
-				console.log('getPropertyValue', key);
-				return pageTheme[key.substr(2)]
-			}
-		}
-	};
+  // init getComputedStyle for weex or too old browser
+  window.getComputedStyle = () => {
+    return {
+      getPropertyValue: (key) => {
+        console.log('getPropertyValue', key);
+        return pageTheme[key.substr(2)]
+      }
+    }
+  };
 }
 
 // set new CSS variables
 export function setCSSVariables(config) {
-	if (isThemeWeb) {
-		for (let key in config) {
-			bodyStyle.setProperty(PREFIX + key, config[key]);
-		}
-	} else if (typeof window === 'object') {
-		pageTheme = {
-	    ...pageTheme,
-	    ...config,
-		};
-	}
+  if (isThemeWeb) {
+    for (let key in config) {
+      bodyStyle.setProperty(PREFIX + key, config[key]);
+    }
+  } else if (typeof window === 'object') {
+    pageTheme = {
+      ...pageTheme,
+      ...config,
+    };
+  }
 }
 
 // set new CSS variable
 export function setCSSVariable(key, value) {
-	if (isThemeWeb) {
-		bodyStyle.setProperty(PREFIX + key, value);
-	} else {
-		pageTheme[key] = value;
-	}
+  if (isThemeWeb) {
+    bodyStyle.setProperty(PREFIX + key, value);
+  } else {
+    pageTheme[key] = value;
+  }
 }
 
 // get CSS variable
 export function getCSSVariable(key) {
-	console.log('getCSSVariable', key);
-	theme = window.getComputedStyle(typeof document == 'object' ? document.body : null);
-	return theme.getPropertyValue(PREFIX + key);
+  const theme = window.getComputedStyle(typeof document == 'object' ? document.body : null);
+  return theme.getPropertyValue(PREFIX + key);
 }
