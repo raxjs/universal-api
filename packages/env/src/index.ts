e9a077376e18784accaad6d21a9fc0bf422d4231
小滐 <wlj136209@alibaba-inc.com>
2020-03-02 15:08:42 +0800

    fix: 隔离快应用和web/weex的判断

--- packages/env/src/index.ts
+++ packages/env/src/index.ts
@@ -9,18 +9,15 @@ function isUndef(type): boolean {
   return type === 'undefined';
 }
 
-export const isQuickApp = !isUndef(typeof global) && global !== null && (!isUndef(global.$app_require$));
-// In quickapp, window exists
-export const isWeb = !isQuickApp && !isUndef(typeof window) && 'onload' in window;
+export const isWeb = !isUndef(typeof window) && 'onload' in window;
 export const isNode =
   !isUndef(typeof process) && !!(process.versions && process.versions.node);
-// In quickapp, callNative exists
 export const isWeex =
-  !isQuickApp &&
-  (!isUndef(typeof callNative) ||
-  !isUndef(typeof WXEnvironment) && WXEnvironment.platform !== 'Web');
+  !isUndef(typeof callNative) ||
+  !isUndef(typeof WXEnvironment) && WXEnvironment.platform !== 'Web';
 export const isKraken = !isUndef(typeof __kraken__);
 export const isMiniApp = !isUndef(typeof my) && my !== null && !isUndef(my.alert);
 // In wechat mini program, wx.login is a function
 // In wechat mini propgram webview, there is no wx.login, but exist wx.miniProgram
 export const isWeChatMiniProgram = !isUndef(typeof wx) && wx !== null && (!isUndef(wx.login) || !isUndef(wx.miniProgram));
+export const isQuickApp = !isUndef(typeof global) && global !== null && (!isUndef(global.$app_require$));
