99be7c7405cc0575520e09e5e2f6e0a18ac209f8
qianxin.yy <qianxin.yy@alibaba-inc.com>
2020-03-05 14:53:15 +0800

    feat:navigate兼容快应用

--- packages/navigate/src/quickapp/index.js
+++ packages/navigate/src/quickapp/index.js
@@ -1,23 +1,38 @@
 import router from '@system.router';
+import qs from 'qs';
 
 export default {
-  push: (param) => {
-    if (param.url && param.url !== '') {
-      param.animated && delete param.animated;
-      let arr = Object.assign({}, param);
-      delete arr.url;
-      router.push({
-        uri: param.url,
-        params: arr,
-      });
-    }
+  push: function push(param) {
+    return new Promise((resolve, reject) => {
+      if (param.url && param.url !== '') {
+        param.animated && delete param.animated;
+        var arr = Object.assign({}, param);
+        delete arr.url;
+        var uri = '';
+        uri = `${param.url}?${qs.stringify(arr)}`;
+        router.push({
+          uri,
+        });
+        resolve('success');
+      } else {
+        reject('lack of url');
+      }
+    });
   },
   go: (param) => {
-    if (param.step === -1) {
-      router.back();
-    }
+    return new Promise((resolve, reject) => {
+      if (param.step === -1) {
+        resolve('success');
+        router.back();
+      } else {
+        reject('Step only supports values of -1');
+      }
+    });
   },
   pop: () => {
-    router.clear();
+    return new Promise((resolve, reject) => {
+      resolve('success');
+      router.clear();
+    });
   }
 };
