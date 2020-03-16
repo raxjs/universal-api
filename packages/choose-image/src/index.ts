21061231099643158c17193e6ddef6bfaa87f6d0
qianxin.yy <qianxin.yy@alibaba-inc.com>
2020-03-16 14:21:15 +0800

    feat:修改引入路径方式

--- packages/choose-image/src/index.ts
+++ packages/choose-image/src/index.ts
@@ -1,8 +1,9 @@
-import { isWeb, isWeex, isMiniApp, isWeChatMiniprogram } from 'universal-env';
+import { isWeb, isWeex, isMiniApp, isWeChatMiniprogram, isQuickApp } from 'universal-env';
 import webModule from './web/index';
 import weexModule from './weex/index';
 import miniAppModule from './miniapp/ali/index';
 import weChatModule from './miniapp/wechat/index';
+import quickAppModule from './quickapp/index';
 
 import { ChooseImage } from './types';
 
@@ -23,6 +24,10 @@ if (isMiniApp) {
 
 if (isWeChatMiniprogram) {
   chooseImage = weChatModule;
+}
+
+if (isQuickApp) {
+  chooseImage = quickAppModule;
 }
 
 export default chooseImage;
