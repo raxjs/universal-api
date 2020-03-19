5672ed2143a8a265ca144e3e4765093b4bcd7f28
qianxin.yy <qianxin.yy@alibaba-inc.com>
2020-03-19 12:00:10 +0800

    asyncstorage兼容快应用

--- packages/asyncstorage/src/index.ts
+++ packages/asyncstorage/src/index.ts
@@ -1,9 +1,8 @@
-import { isWeb, isWeex, isMiniApp, isWeChatMiniprogram, isQuickApp } from 'universal-env';
+import { isWeb, isWeex, isMiniApp, isWeChatMiniProgram } from 'universal-env';
 import webModule from './web/index';
 import weexModule from './weex/index';
 import miniAppModule from './miniapp/ali/index';
 import weChatModule from './miniapp/wechat/index';
-import quickAppModule from './quickapp/index';
 import { AsyncStorage } from './types';
 
 let AsyncStorage: AsyncStorage;
@@ -20,12 +19,8 @@ if (isMiniApp) {
   AsyncStorage = miniAppModule;
 }
 
-if (isWeChatMiniprogram) {
+if (isWeChatMiniProgram) {
   AsyncStorage = weChatModule;
-}
-
-if (isQuickApp) {
-  AsyncStorage = quickAppModule;
 }
 
 export default AsyncStorage;
