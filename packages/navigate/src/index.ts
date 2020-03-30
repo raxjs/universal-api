fd23a9178ed9665d2c882b18a9255a651e3e6c13
wlj136209 <wlj136209@alibaba-inc.com>
2020-03-30 20:02:35 +0800

    feat: support bytedance

--- packages/navigate/src/index.ts
+++ packages/navigate/src/index.ts
@@ -1,8 +1,9 @@
-import { isWeb, isWeex, isMiniApp, isWeChatMiniProgram } from 'universal-env';
+import { isWeb, isWeex, isMiniApp, isWeChatMiniProgram, isByteDanceMicroApp } from 'universal-env';
 import webModule from './web/index';
 import weexModule from './weex/index';
 import miniAppModule from './miniapp/ali/index';
 import weChatModule from './miniapp/wechat/index';
+import byteDanceModule from './miniapp/byteDance/index';
 
 import { INavigate } from './types';
 
@@ -43,6 +44,13 @@ function handleWeChat() {
   return null;
 }
 
+function handleByteDance() {
+  if (isByteDanceMicroApp) {
+    return byteDanceModule;
+  }
+  return null;
+}
+
 // Default module is web
 function handleDefault() {
   return webModule;
@@ -53,6 +61,7 @@ const Navigate: INavigate = dutyChain(
   handleWeex,
   handleMiniApp,
   handleWeChat,
+  handleByteDance,
   handleDefault
 );
 
