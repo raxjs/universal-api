d72e9f65c5eca81b24b6e9204d835735f88ae862
ruanxiaoquan <xiaoquan.rxq@alibaba-inc.com>
2020-03-31 15:48:10 +0800

    ci:eslint

--- packages/toast/src/quickapp/index.ts
+++ packages/toast/src/quickapp/index.ts
@@ -1,4 +1,4 @@
-import { ToastOption } from "../types";
+import { ToastOption } from '../types';
 
 const LONG_DELAY = 1;
 const SHORT_DELAY = 0;
@@ -11,7 +11,8 @@ const Toast: ToastOption = {
    * @param duration {Number}
    */
   show(message: string, duration: number = SHORT_DELAY): void {
-    const prompt = require("@system.prompt");
+    // eslint-disable-next-line  @typescript-eslint/no-var-requires
+    const prompt = require('@system.prompt');
     prompt.showToast({
       message,
       duration
