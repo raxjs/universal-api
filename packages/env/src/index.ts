c000242d46ab56fbf176124e782b949a3451072e
小杰 <wlj136209@alibaba-inc.com>
2020-03-31 21:05:39 +0800

    Fix: exclude tt in judgement of wechat mini program (#65)
    * feat: support isByteDance
    
    * fix: beta version
    
    * fix: isByteDanceMicroApp
    
    * feat: exclude tt in isWeChatMiniProgram
    
    * feat: exclude tt in isWeChatMiniProgram
    
    * fix: version

--- packages/env/src/index.ts
+++ packages/env/src/index.ts
@@ -14,8 +14,9 @@ export const isNode = !isUndef(typeof process) && !!(process.versions && process
 export const isWeex = !isUndef(typeof WXEnvironment) && WXEnvironment.platform !== 'Web';
 export const isKraken = !isUndef(typeof __kraken__);
 export const isMiniApp = !isUndef(typeof my) && my !== null && !isUndef(typeof my.alert);
+export const isByteDanceMicroApp = !isUndef(typeof tt) && tt !== null && !isUndef(typeof tt) && !isUndef(tt.showToast);
 // In wechat mini program, wx.login is a function
 // In wechat mini propgram webview, there is no wx.login, but exist wx.miniProgram
-export const isWeChatMiniProgram = !isUndef(typeof wx) && wx !== null && (!isUndef(typeof wx.login) || !isUndef(typeof wx.miniProgram));
+// In bytedance maicro app, there is wx variable.
+export const isWeChatMiniProgram = !isByteDanceMicroApp && !isUndef(typeof wx) && wx !== null && (!isUndef(typeof wx.login) || !isUndef(typeof wx.miniProgram));
 export const isQuickApp = !isUndef(typeof global) && global !== null && !isUndef(typeof global.callNative) && !isWeex;
-export const isByteDanceMicroApp = !isUndef(typeof tt) && tt !== null && !isUndef(typeof tt) && !isUndef(tt.showToast);
