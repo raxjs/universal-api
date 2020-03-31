2126b03b5e58e07acfe244e31f48dcff8c7d221c
ruanxiaoquan <xiaoquan.rxq@alibaba-inc.com>
2020-03-31 15:55:12 +0800

    ci:eslint

--- packages/alert/src/quickapp/index.ts
+++ packages/alert/src/quickapp/index.ts
@@ -1,14 +1,15 @@
-import { Options } from "../types";
+import { Options } from '../types';
 
 const alert = (options: Options = {}): Promise<null> => {
-  const { title = "", content = "", buttonText = "确定" } = options;
+  const { title = '', content = '', buttonText = '确定' } = options;
   const buttons = [
     {
       text: buttonText
     }
   ];
   return new Promise((resolve, reject) => {
-    const prompt = require("@system.prompt");
+    // eslint-disable-next-line @typescript-eslint/no-var-requires
+    const prompt = require('@system.prompt');
     prompt.showDialog({
       title,
       message: content,
