f13ef5c75a5d7444baadfa9c08accd170e14480e
ruanxiaoquan <xiaoquan.rxq@alibaba-inc.com>
2020-04-07 11:50:40 +0800

    chore:format code

--- packages/alert/src/index.ts
+++ packages/alert/src/index.ts
@@ -1,11 +1,11 @@
-import { isWeb, isWeex, isMiniApp, isWeChatMiniProgram ,isQuickApp} from "universal-env";
-import webModule from "./web/index";
-import weexModule from "./weex/index";
-import miniAppModule from "./miniapp/ali/index";
-import weChatModule from "./miniapp/wechat/index";
-import quickAppModule from "./quickapp/index";
+import { isWeb, isWeex, isMiniApp, isWeChatMiniProgram ,isQuickApp} from 'universal-env';
+import webModule from './web/index';
+import weexModule from './weex/index';
+import miniAppModule from './miniapp/ali/index';
+import weChatModule from './miniapp/wechat/index';
+import quickAppModule from './quickapp/index';
 
-import { Alert } from "./types";
+import { Alert } from './types';
 
 let alert: Alert = () => Promise.resolve(null);
 if (isWeb) {
