6ba1648e48c2fef4c354489c2e65358f3b1cc05c
siteng.xst <siteng.xst@alibaba-inc.com>
2020-04-08 19:17:22 +0800

    feat: toast support bytedance-microapp

--- packages/toast/src/index.ts
+++ packages/toast/src/index.ts
@@ -1,8 +1,9 @@
-import { isWeex, isMiniApp, isWeb, isWeChatMiniProgram } from 'universal-env';
+import { isWeex, isMiniApp, isWeb, isWeChatMiniProgram, isByteDanceMicroApp } from 'universal-env';
 import webModule from './web/index';
 import weexModule from './weex/index';
 import miniAppModule from './miniapp/ali/index';
 import weChatModule from './miniapp/wechat/index';
+import byteDanceModule from './miniapp/bytedance/index';
 import { ToastOption } from './types';
 
 let Toast: ToastOption = {} as any;
@@ -16,6 +17,8 @@ if (isWeb) {
   Toast = miniAppModule;
 } else if (isWeChatMiniProgram) {
   Toast = weChatModule;
+} else if (isByteDanceMicroApp) {
+  Toast = byteDanceModule;
 } else {
   // Web as default
   Toast = webModule;
