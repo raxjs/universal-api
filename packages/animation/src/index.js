0fb1cc670d942d68497c3c31c8735070d8fd4898
broven <zycode277@gmail.com>
2020-04-09 10:44:20 +0800

    feat: animation for bytedance

--- packages/animation/src/index.js
+++ packages/animation/src/index.js
@@ -1,13 +1,13 @@
 'use strict';
-import { isWeb, isWeex, isMiniApp, isWeChatMiniProgram } from 'universal-env';
+import { isWeb, isWeex, isMiniApp, isWeChatMiniProgram, isByteDanceMicroApp } from 'universal-env';
 import binding from 'weex-bindingx';
-import transition from 'universal-transition';
+import transition from 'tt-universal-transition';
 import transformProperty from './transformProperty';
 import transformEasing from './transformEasing';
 import formatBezier from './formatBezier';
 import { find, map, forEach } from './utils';
 
-const inMiniApp = isMiniApp || isWeChatMiniProgram;
+const inMiniApp = isMiniApp || isWeChatMiniProgram || isByteDanceMicroApp;
 
 function getSupportBinding() {
   return binding && binding.isSupportNewBinding;
