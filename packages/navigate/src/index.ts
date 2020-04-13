b4bb06361a9aca62b22dfb25aff16ebed96d701a
qianxin.yy <qianxin.yy@alibaba-inc.com>
2020-04-13 20:23:04 +0800

    fix：fix @system

--- packages/navigate/src/index.ts
+++ packages/navigate/src/index.ts
@@ -3,7 +3,6 @@ import webModule from './web/index';
 import weexModule from './weex/index';
 import miniAppModule from './miniapp/ali/index';
 import weChatModule from './miniapp/wechat/index';
-import quickAppModule from './quickapp/index';
 import { INavigate } from './types';
 
 function dutyChain(...fns) {
@@ -38,6 +37,7 @@ function handleMiniApp() {
 
 function handleQuickApp() {
   if (isQuickApp) {
+    const quickAppModule = require('./quickapp/index');
     return quickAppModule;
   }
   return null;
