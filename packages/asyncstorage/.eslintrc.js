cc560d8e9127b96fda0eea7c406ff0a27ba78646
qianxin.yy <qianxin.yy@alibaba-inc.com>
2020-04-14 16:54:37 +0800

    fix: modify eslintrc

--- packages/asyncstorage/.eslintrc.js
+++ packages/asyncstorage/.eslintrc.js
@@ -2,9 +2,7 @@ module.exports = {
   extends: ['eslint-config-rax/react', 'eslint-config-rax/typescript'],
   globals: {},
   rules: {
-    '@typescript-eslint/interface-name-prefix': ['off', ''],
-    '@typescript-eslint/no-explicit-any': 'off',
-    '@typescript-eslint/explicit-function-return-type': 'off',
-    'quotes': ['error', 'single']
+    "@typescript-eslint/no-explicit-any": "off",
+    "@typescript-eslint/explicit-function-return-type": "off"
   }
 };
