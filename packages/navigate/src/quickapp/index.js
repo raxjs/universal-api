5498919541ae33350df990c982d289dd1117c2c4
qianxin.yy <qianxin.yy@alibaba-inc.com>
2020-04-15 11:15:01 +0800

    feat:add quickpp for navigate

---
+++ packages/navigate/src/quickapp/index.js
@@ -0,0 +1,36 @@
+export default {
+  push: (param) => {
+    return new Promise((resolve, reject) => {
+      // eslint-disable-next-line @typescript-eslint/no-var-requires
+      const router = require('@system.router');
+      if (param.url && param.url !== '') {
+        router.push({
+          uri: param.url,
+        });
+        resolve('success');
+      } else {
+        reject('lack of url');
+      }
+    });
+  },
+  go: (param) => {
+    return new Promise((resolve, reject) => {
+      // eslint-disable-next-line @typescript-eslint/no-var-requires
+      const router = require('@system.router');
+      if (param.step === -1) {
+        resolve('success');
+        router.back();
+      } else {
+        reject('Step only supports values of -1');
+      }
+    });
+  },
+  pop: () => {
+    return new Promise((resolve, reject) => {
+      // eslint-disable-next-line @typescript-eslint/no-var-requires
+      const router = require('@system.router');
+      resolve('success');
+      router.back();
+    });
+  }
+};
