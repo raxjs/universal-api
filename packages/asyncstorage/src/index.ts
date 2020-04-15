da6f879f335f95d635bcef7ef6501609e14e0172
broven <zycode277@gmail.com>
2020-04-15 16:23:13 +0800

    chore: remove  quickapp

--- packages/asyncstorage/src/index.ts
+++ packages/asyncstorage/src/index.ts
@@ -1,18 +1,15 @@
-import { isWeb, isWeex, isMiniApp, isWeChatMiniProgram, isByteDanceMicroApp, isQuickApp } from 'universal-env';
+import { isWeb, isWeex, isMiniApp, isWeChatMiniProgram, isByteDanceMicroApp } from 'universal-env';
 
 import webModule from './web/index';
 import weexModule from './weex/index';
 import miniAppModule from './miniapp/ali/index';
 import weChatModule from './miniapp/wechat/index';
 import bytedanceModule from './miniapp/bytedance/index';
-import quickModule from './quickapp/index';
 
 import { AsyncStorage } from './types';
 
 let AsyncStorage: AsyncStorage;
-if (isQuickApp) {
-  AsyncStorage = quickModule;
-} else if (isWeb) {
+if (isWeb) {
   AsyncStorage = webModule;
 } else if (isWeex) {
   AsyncStorage = weexModule;
