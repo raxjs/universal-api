41f5b351040a27d1c9d908907e8cf009060847a5
siteng.xst <siteng.xst@alibaba-inc.com>
2020-04-20 16:54:30 +0800

    fix: directory unification

--- packages/toast/src/index.ts
+++ packages/toast/src/index.ts
@@ -1,9 +1,9 @@
 import { isWeex, isMiniApp, isWeb, isWeChatMiniProgram, isByteDanceMicroApp } from 'universal-env';
 import webModule from './web/index';
 import weexModule from './weex/index';
-import miniAppModule from './miniapp/ali/index';
-import weChatModule from './miniapp/wechat/index';
-import byteDanceModule from './miniapp/bytedance/index';
+import miniAppModule from './miniapp/ali-miniapp/index';
+import weChatModule from './miniapp/wechat-miniprogram/index';
+import byteDanceModule from './miniapp/bytedance-microapp/index';
 import { ToastOption } from './types';
 
 let Toast: ToastOption = {} as any;
