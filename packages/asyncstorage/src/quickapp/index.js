9bb2d3a1f6fe6bc46b291ef64ac1729503b0bd49
qianxin.yy <qianxin.yy@alibaba-inc.com>
2020-04-27 11:53:25 +0800

    fix: export

--- packages/asyncstorage/src/quickapp/index.js
+++ packages/asyncstorage/src/quickapp/index.js
@@ -3,74 +3,77 @@ import otherModule from '../index';
 
 let exportModule;
 
-const quickModule = {
-  setItem: function(key, value) {
-    return new Promise(function(resolve, reject) {
-      // eslint-disable-next-line @typescript-eslint/no-var-requires
-      const storage = require('@system.storage');
-      storage.set({
-        key,
-        value,
-        success: function(data) {
-          resolve(data);
-        },
-        fail: function(data, code) {
-          reject(`handling fail, code = ${code}`);
-        }
-      });
+function setItem(key, value) {
+  return new Promise(function(resolve, reject) {
+    // eslint-disable-next-line @typescript-eslint/no-var-requires
+    const storage = require('@system.storage');
+    storage.set({
+      key,
+      value,
+      success: function(data) {
+        resolve(data);
+      },
+      fail: function(data, code) {
+        reject(`handling fail, code = ${code}`);
+      }
     });
-  },
+  });
+}
 
-  getItem: function(key) {
-    return new Promise(function(resolve, reject) {
-      // eslint-disable-next-line @typescript-eslint/no-var-requires
-      const storage = require('@system.storage');
-      storage.get({
-        key,
-        success: function(data) {
-          resolve(data);
-        },
-        fail: function(data, code) {
-          reject(`handling fail, code = ${code}`);
-        }
-      });
+function getItem(key) {
+  return new Promise(function(resolve, reject) {
+    // eslint-disable-next-line @typescript-eslint/no-var-requires
+    const storage = require('@system.storage');
+    storage.get({
+      key,
+      success: function(data) {
+        resolve(data);
+      },
+      fail: function(data, code) {
+        reject(`handling fail, code = ${code}`);
+      }
     });
-  },
+  });
+}
 
-  removeItem: function(key) {
-    return new Promise(function(resolve, reject) {
-      // eslint-disable-next-line @typescript-eslint/no-var-requires
-      const storage = require('@system.storage');
-      storage.delete({
-        key,
-        success: function(data) {
-          resolve(data);
-        },
-        fail: function(data, code) {
-          reject(`handling fail, code = ${code}`);
-        }
-      });
+function removeItem(key) {
+  return new Promise(function(resolve, reject) {
+    // eslint-disable-next-line @typescript-eslint/no-var-requires
+    const storage = require('@system.storage');
+    storage.delete({
+      key,
+      success: function(data) {
+        resolve(data);
+      },
+      fail: function(data, code) {
+        reject(`handling fail, code = ${code}`);
+      }
     });
-  },
+  });
+}
 
-  clear: function() {
-    return new Promise(function(resolve, reject) {
-      // eslint-disable-next-line @typescript-eslint/no-var-requires
-      const storage = require('@system.storage');
-      storage.clear({
-        success: function(data) {
-          resolve(data);
-        },
-        fail: function(data, code) {
-          reject(`handling fail, code = ${code}`);
-        }
-      });
+function clear() {
+  return new Promise(function(resolve, reject) {
+    // eslint-disable-next-line @typescript-eslint/no-var-requires
+    const storage = require('@system.storage');
+    storage.clear({
+      success: function(data) {
+        resolve(data);
+      },
+      fail: function(data, code) {
+        reject(`handling fail, code = ${code}`);
+      }
     });
-  }
+  });
 }
 
 if (isQuickApp) {
-  exportModule = quickModule;
+  exportModule = {
+    setItem,
+    getItem,
+    removeItem,
+    clear
+  };
 } else {
   exportModule = otherModule;
 }
