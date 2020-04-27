d9bb551e0aa43510358318bf5d228065ec311c5f
qianxin.yy <qianxin.yy@alibaba-inc.com>
2020-04-27 11:54:28 +0800

    fix: export

--- packages/navigate/src/quickapp/index.js
+++ packages/navigate/src/quickapp/index.js
@@ -3,45 +3,49 @@ import otherModule from '../index';
 
 let exportModule;
 
-const quickModule = {
-  push: (param) => {
-    return new Promise((resolve, reject) => {
-      // eslint-disable-next-line @typescript-eslint/no-var-requires
-      const router = require('@system.router');
-      if (param.url && param.url !== '') {
-        router.push({
-          uri: param.url,
-        });
-        resolve('success');
-      } else {
-        reject('lack of url');
-      }
-    });
-  },
-  go: (param) => {
-    return new Promise((resolve, reject) => {
-      // eslint-disable-next-line @typescript-eslint/no-var-requires
-      const router = require('@system.router');
-      if (param.step === -1) {
-        resolve('success');
-        router.back();
-      } else {
-        reject('Step only supports values of -1');
-      }
-    });
-  },
-  pop: () => {
-    return new Promise((resolve, reject) => {
-      // eslint-disable-next-line @typescript-eslint/no-var-requires
-      const router = require('@system.router');
+function push(param) {
+  return new Promise((resolve, reject) => {
+    // eslint-disable-next-line @typescript-eslint/no-var-requires
+    const router = require('@system.router');
+    if (param.url && param.url !== '') {
+      router.push({
+        uri: param.url,
+      });
+      resolve('success');
+    } else {
+      reject('lack of url');
+    }
+  });
+}
+
+function go(param) {
+  return new Promise((resolve, reject) => {
+    // eslint-disable-next-line @typescript-eslint/no-var-requires
+    const router = require('@system.router');
+    if (param.step === -1) {
       resolve('success');
       router.back();
-    });
-  }
-};
+    } else {
+      reject('Step only supports values of -1');
+    }
+  });
+}
+
+function pop() {
+  return new Promise((resolve, reject) => {
+    // eslint-disable-next-line @typescript-eslint/no-var-requires
+    const router = require('@system.router');
+    resolve('success');
+    router.back();
+  });
+}
 
 if (isQuickApp) {
-  exportModule = quickModule;
+  exportModule = {
+    push,
+    go,
+    pop
+  };
 } else {
   exportModule = otherModule;
 }
