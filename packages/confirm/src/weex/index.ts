8fdd49717db2cbdc12e549d2fd2aa36526353025
fushen <fushen.jzw@alibaba-inc.com>
2020-05-07 17:38:53 +0800

    fix: module name

--- packages/confirm/src/weex/index.ts
+++ packages/confirm/src/weex/index.ts
@@ -6,7 +6,7 @@ declare const __weex_require__: any;
 const confirm = (options: Options): Promise<boolean> => {
   return new Promise((resolve): void => {
     const { content = '', confirmButtonText, cancelButtonText } = options;
-    const modal = __weex_require__('modal');
+    const modal = __weex_require__('@weex-module/modal');
     modal.confirm({
       message: content,
       okTitle: confirmButtonText,
