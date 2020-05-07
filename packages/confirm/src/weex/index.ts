c4296690391f541f6158e7dbf59e24ec9ecde652
fushen <fushen.jzw@alibaba-inc.com>
2020-05-07 17:34:11 +0800

    chore: remove useless code

--- packages/confirm/src/weex/index.ts
+++ packages/confirm/src/weex/index.ts
@@ -6,7 +6,6 @@ declare const __weex_require__: any;
 const confirm = (options: Options): Promise<boolean> => {
   return new Promise((resolve): void => {
     const { content = '', confirmButtonText, cancelButtonText } = options;
-    // @typescript-eslint/camelcase
     const modal = __weex_require__('modal');
     modal.confirm({
       message: content,
