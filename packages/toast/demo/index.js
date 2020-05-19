7038bc6d36389ebe1bb71ae99625e97eeec0cff6
wlj136209 <wlj136209@alibaba-inc.com>
2020-05-19 18:03:20 +0800

    fix: rollback demo

---
+++ packages/toast/demo/index.js
@@ -0,0 +1,20 @@
+/* eslint-disable import/no-extraneous-dependencies */
+// @ts-ignore
+import { createElement, render } from 'rax';
+import DriverUniversal from 'driver-universal';
+import View from 'rax-view';
+import Text from 'rax-text';
+import Toast from '../src/';
+
+const App = () => {
+  const handleClick = () => {
+    Toast.show('Hi');
+  };
+  return (
+    <View onClick={handleClick}>
+      <Text>click it!</Text>
+    </View>
+  );
+};
+
+render(<App />, document.body, { driver: DriverUniversal });
