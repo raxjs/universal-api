7111814c42e74893dfc5a1797bc947dff903cff8
狒狒神 <350740249@qq.com>
2020-06-29 14:15:29 +0800

    fix(confirm): weex require module (#93)
    * fix: weex require module
    
    * chore: remove useless code
    
    * fix: module name
    
    * chore: bump version

--- packages/confirm/src/weex/index.ts
+++ packages/confirm/src/weex/index.ts
@@ -1,21 +1,19 @@
 import { Options } from '../types';
 
-declare const weex: any;
+// eslint-disable-next-line
+declare const __weex_require__: any;
 
 const confirm = (options: Options): Promise<boolean> => {
   return new Promise((resolve): void => {
     const { content = '', confirmButtonText, cancelButtonText } = options;
-    const modal = weex.requireModule('modal');
-    modal.confirm(
-      {
-        message: content,
-        okTitle: confirmButtonText,
-        cancelTitle: cancelButtonText
-      },
-      (confirm: string): void => {
-        confirm === confirmButtonText ? resolve(true) : resolve(false);
-      }
-    );
+    const modal = __weex_require__('@weex-module/modal');
+    modal.confirm({
+      message: content,
+      okTitle: confirmButtonText,
+      cancelTitle: cancelButtonText,
+    }, (confirm: string): void => {
+      confirm === confirmButtonText ? resolve(true) : resolve(false);
+    });
   });
 };
 
