0279b24e1cbf35b9aa2732e00062a86786d6dcb9
逆葵 <xianyong.yxy@alibaba-inc.com>
2020-08-05 14:47:56 +0800

    fix(image): use filePaths in compress image in wechat

--- packages/image/src/miniapp/wechat/compress.ts
+++ packages/image/src/miniapp/wechat/compress.ts
@@ -2,5 +2,5 @@ import promisifyFn from '../promisifyFn';
 
 declare const wx: any;
 export default options => {
-  return promisifyFn(wx.compressImage, options, null, null);
+  return promisifyFn(wx.compressImage, options, {'filePaths': 'tempFilePath'}, null);
 };
