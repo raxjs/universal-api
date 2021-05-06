43437ee983a48af046e3633d09b89019d92305f2
旺山 <mazhenwen.mzw@alibaba-inc.com>
2021-05-06 15:01:53 +0800

    fix: 删除一些无用代码

--- src/packages/interactive/pullDownRefresh/src/common.ts
+++ src/packages/interactive/pullDownRefresh/src/common.ts
@@ -3,18 +3,6 @@ import { StartOptions, StopOptions } from './types';
 import { promisify } from '@utils/promisify';
 import { styleIn } from '@utils/styleOptions';
 
-// function styleOptions(options: StartOptions) {
-//   if (!options) {
-//     options = {};
-//   }
-//   if (isWeChatMiniProgram || isByteDanceMicroApp) {
-//     // 在微信和字节小程序中title是必选项，但是可以传空字符串，微信可能存在mask显示透明蒙层，防止触摸穿透的问题，测试的时候看下
-//     options.title = options.content || '';
-//     delete options.content;
-//   }
-//   return options;
-// }
-
 /**
  * startPullDownRefresh api
  * @param api
