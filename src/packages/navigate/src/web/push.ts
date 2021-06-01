4313c7caebe577ac90e92fb58f7519d2e001e891
yitan.lgh <yitan.lgh@alibaba-inc.com>
2021-06-01 10:53:35 +0800

    fix: 修复web push hash 实现

--- src/packages/navigate/src/web/push.ts
+++ src/packages/navigate/src/web/push.ts
@@ -4,17 +4,19 @@ import { CONTAINER_NAME } from '@utils/constant';
 
 export const push = normalize.push((options: IPushOptions) => {
   const { url, isHash = false, refresh = true, success, fail, complete } = options;
-  const _url = isHash ?
-    `${location.protocol}//${location.host}/${location.pathname.split('/').filter((i) => !!i).join('/')}${location.search ? location.search : '/'}#${url}` : url;
+  // const _url = isHash ?
+  //   `${location.protocol}//${location.host}/${location.pathname.split('/').filter((i) => !!i).join('/')}${location.search ? location.search : '/'}#${url}` : url;
   setTimeout((): void => {
     try {
-      if (refresh) {
-        location.href = _url;
+      if (isHash) {
+        location.hash = `#${url}`;
+      } else if (refresh) {
+        location.href = url;
       } else {
         const state = { page_id: 1 };
         const title = '';
 
-        history.pushState(state, title, _url);
+        history.pushState(state, title, url);
       }
 
       success && success();
