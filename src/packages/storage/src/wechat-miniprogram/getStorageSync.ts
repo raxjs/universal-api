4c856d2abd7c0224f95ea30a1922df3d49e4ad62
阿进 <chenhongjin.chj@alibaba-inc.com>
2021-06-10 11:10:09 +0800

    fix: getStorage res type

--- src/packages/storage/src/wechat-miniprogram/getStorageSync.ts
+++ src/packages/storage/src/wechat-miniprogram/getStorageSync.ts
@@ -1,8 +1,8 @@
-import { GetOrRemoveSyncOptionStruct } from '../types';
+import { GetOrRemoveSyncOptionStruct, GetStorageSyncRes } from '../types';
 import { styleIn } from '@utils/styleOptions';
 import { CONTAINER_NAME } from '@utils/constant';
 
-const getStorageSync = (args: GetOrRemoveSyncOptionStruct) => {
+const getStorageSync = (args: GetOrRemoveSyncOptionStruct): GetStorageSyncRes => {
   args = styleIn(args, CONTAINER_NAME.WECHAT);
   return { data: wx.getStorageSync(args.key) };
 };
