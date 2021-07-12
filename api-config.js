30d3369846365d657d9660a60141f1451baeb494
旺山 <mazhenwen.mzw@alibaba-inc.com>
2021-07-12 14:49:06 +0800

    fix: nabigationBar版本和大包版本+1

--- api-config.js
+++ api-config.js
@@ -179,7 +179,7 @@ module.exports = {
     path: 'src/packages/interactive/navigationBar/src/index.ts',
     pkgInfo: [
       {
-        version: '1.0.0',
+        version: '1.0.1',
         name: '@uni/navigation-bar',
       },
     ],
