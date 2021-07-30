ab5af2fc7e353bed58829692489262dc17052d57
旺山 <mazhenwen.mzw@alibaba-inc.com>
2021-07-30 19:32:34 +0800

    test: add test to setNavigationBar

--- src/packages/interactive/navigationBar/__test__/api.test.ts
+++ src/packages/interactive/navigationBar/__test__/api.test.ts
@@ -1,22 +1,37 @@
 import { createPromisifyImpl, isAliContainer, testPlatformAPI } from '@utils/__test__/util';
 
-testPlatformAPI('navigationBar', ['wechat', 'bytedance', 'kuaishou', 'baidu'], async (container, globals, configAPI) => {
+testPlatformAPI('navigationBar', ['wechat', 'ali', 'dingtalk', 'bytedance', 'kuaishou', 'baidu'], async (container, globals, configAPI) => {
+  const mockSetNavigationBar = jest.fn(createPromisifyImpl());
   const mockSetNavigationBarColor = jest.fn(createPromisifyImpl());
   const mockSetNavigationBarTitle = jest.fn(createPromisifyImpl());
 
   // TODO 临时模拟 window，待删除
   globals.window = {};
-  configAPI('setNavigationBarColor', mockSetNavigationBarColor);
-  configAPI('setNavigationBarTitle', mockSetNavigationBarTitle);
+  if (isAliContainer(container)) {
+    configAPI('setNavigationBar', mockSetNavigationBar);
+  } else {
+    configAPI('setNavigationBarColor', mockSetNavigationBarColor);
+    configAPI('setNavigationBarTitle', mockSetNavigationBarTitle);
+  }
 
   const { setNavigationBarColor, setNavigationBarTitle } = require('../src/index');
   await setNavigationBarColor({
     backgroundColor: '#ff0000',
   });
-  expect(mockSetNavigationBarColor.mock.calls.length).toBe(1);
-  expect(mockSetNavigationBarColor.mock.calls[0][0].backgroundColor).toBe('#ff0000');
+  if (isAliContainer(container)) {
+    expect(mockSetNavigationBar.mock.calls.length).toBe(1);
+    expect(mockSetNavigationBar.mock.calls[0][0].backgroundColor).toBe('#ff0000');
+  } else {
+    expect(mockSetNavigationBarColor.mock.calls.length).toBe(1);
+    expect(mockSetNavigationBarColor.mock.calls[0][0].backgroundColor).toBe('#ff0000');
+  }
 
   await setNavigationBarTitle({ title: 'abc' });
-  expect(mockSetNavigationBarTitle.mock.calls.length).toBe(1);
-  expect(mockSetNavigationBarTitle.mock.calls[0][0].title).toBe('abc');
+  if (isAliContainer(container)) {
+    expect(mockSetNavigationBar.mock.calls.length).toBe(2);
+    expect(mockSetNavigationBar.mock.calls[1][0].title).toBe('abc');
+  } else {
+    expect(mockSetNavigationBarTitle.mock.calls.length).toBe(1);
+    expect(mockSetNavigationBarTitle.mock.calls[0][0].title).toBe('abc');
+  }
 });
