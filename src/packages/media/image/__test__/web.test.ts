4e1b66edf07712796a2f56de7160c44f7ad54327
peakchen90 <1558267774@qq.com>
2021-08-31 17:08:06 +0800

    chore: add web test for `location`

---
+++ src/packages/media/image/__test__/web.test.ts
@@ -0,0 +1,7 @@
+import { testWebAPI } from '@/utils/__test__/util';
+
+testWebAPI('image', async (globals) => {
+  const { chooseLocation, openLocation, getLocation } = require('../src/index');
+
+
+});
