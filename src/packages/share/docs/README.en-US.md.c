6ab46ea664d6fcdc8c79e85fb7368fd99e768ab2
汉堡鸡肉卷鸡腿鸡翅 <2leg@immaster.cn>
2021-08-31 17:16:51 +0800

    docs: update share & open docs (#228)
    Co-authored-by: yitan.lgh <yitan.lgh@alibaba-inc.com>

--- src/packages/share/docs/README.en-US.md.c
+++ src/packages/share/docs/README.en-US.md.c
@@ -1,9 +1,3 @@
----
-group:
-  title: Open Capabilities
-title: authorize
----
-
 # authorize 
 
 [![npm](https://img.shields.io/npm/v/@uni/authorize.svg)](https://www.npmjs.com/package/@uni/authorize)
@@ -17,9 +11,6 @@ When such an API is called:
 - If the user has accepted authorization, the API can be called directly;
 - If the user has rejected authorization, no pop-up appears. Instead, API fail callback will be accessed directly. Developers should make the scenario compatible where the user has rejected to authorization.
 
-
-<div style="display: flex;flex-direction: row;justify-content: space-between;">
-<div style="margin-right: 20px;max-width: 50%;">
 
 ## Supported
 
@@ -99,29 +90,47 @@ Note: only Alipay applet returns.
 | response.authErrorScopes | `object`  | Failed authorization type. Key is the scope of failed authorization, and value is the corresponding error code |
 | response.authSuccessScopes | `Array`  | Successful authorization scope |
 
-</div>
-<div>
 
-```jsx | inline
-/**
- * iframe: true
- */
-import React from 'react';
-export default () => (
-  <iframe style={{
-      boxShadow: '0 2px 15px rgba(0,0,0,0.1)',
-      width: '375px',
-      height: '700px'
-    }} src='https://herbox-embed.alipay.com/p/uni/uni?previewZoom=100&view=preview&defaultPage=pages/authorize/index&topSlider=false'></iframe>
-);
+# hideShareMenu 
+
+[![npm](https://img.shields.io/npm/v/@uni/share.svg)](https://www.npmjs.com/package/@uni/share)
+
+Hides the Forward button on the current page
+
+## Supported
+
+<img alt="miniApp" src="https://gw.alicdn.com/tfs/TB1bBpmbRCw3KVjSZFuXXcAOpXa-200-200.svg" width="25px" height="25px" title="阿里小程序" /> <img alt="wechatMiniprogram" src="https://img.alicdn.com/tfs/TB1slcYdxv1gK0jSZFFXXb0sXXa-200-200.svg" width="25px" height="25px" title="微信小程序" /> <img alt="bytedanceMicroApp" src="https://gw.alicdn.com/tfs/TB1jFtVzO_1gK0jSZFqXXcpaXXa-200-200.svg" width="25px" height="25px" title="字节跳动小程序" /> <img alt="kuaiShouMiniProgram" src="https://gw.alicdn.com/imgextra/i4/O1CN01kzmJMM24jcFEzp5Wv_!!6000000007427-2-tps-200-200.png" width="25px" height="25px" title="快手小程序" />
+
+## Install
+
+```bash
+$ npm install @uni/share --save
+```
+or
+```bash
+$ npm install @uni/apis --save
+```
+## Usage
+
+```javascript
+import { hideShareMenu } from '@uni/share';
+
+hideShareMenu();
 ```
 
-<div style="display: flex;margin-top: 50px;">
-  <div>
-    <img src="https://img.alicdn.com/imgextra/i1/O1CN01Ky8HXS23ytpewgaAV_!!6000000007325-0-tps-688-630.jpg" width="220" height="200" />
-    <div style="text-align: center;">微信小程序</div>
-  </div>
-</div>
+你也可以从大包引入：
+```javascript
+import { share } from '@uni/apis';
+```
 
-</div>
-</div>
+## Parameters
+
+| Property | Type | Description | Required | Default | Supported |
+| --- | --- | --- | --- | --- | --- |
+| options | `object`  |  | No | - | - | <img alt="wechatMiniprogram" src="https://img.alicdn.com/tfs/TB1slcYdxv1gK0jSZFFXXb0sXXa-200-200.svg" width="25px" height="25px" title="微信小程序" /> <img alt="bytedanceMicroApp" src="https://gw.alicdn.com/tfs/TB1jFtVzO_1gK0jSZFqXXcpaXXa-200-200.svg" width="25px" height="25px" title="字节跳动小程序" /> <img alt="kuaiShouMiniProgram" src="https://gw.alicdn.com/imgextra/i4/O1CN01kzmJMM24jcFEzp5Wv_!!6000000007427-2-tps-200-200.png" width="25px" height="25px" title="快手小程序" /> |
+| options.menus | `Array.<string>`  | This interface is a Beta version and is only supported on the Android platform for the time being. The list of forward button names that need to be displayed, the default is ['shareAppMessage']. The legal values ​​of the button name include "shareAppMessage" and "shareTimeline" | No | - | <img alt="wechatMiniprogram" src="https://img.alicdn.com/tfs/TB1slcYdxv1gK0jSZFFXXb0sXXa-200-200.svg" width="25px" height="25px" title="微信小程序" /> |
+| options.success | `Function`  | The callback function for a successful API call | No | - | <img alt="wechatMiniprogram" src="https://img.alicdn.com/tfs/TB1slcYdxv1gK0jSZFFXXb0sXXa-200-200.svg" width="25px" height="25px" title="微信小程序" /> <img alt="bytedanceMicroApp" src="https://gw.alicdn.com/tfs/TB1jFtVzO_1gK0jSZFqXXcpaXXa-200-200.svg" width="25px" height="25px" title="字节跳动小程序" /> <img alt="kuaiShouMiniProgram" src="https://gw.alicdn.com/imgextra/i4/O1CN01kzmJMM24jcFEzp5Wv_!!6000000007427-2-tps-200-200.png" width="25px" height="25px" title="快手小程序" /> |
+| options.fail | `Function`  | The callback function for a failed API call | No | - | <img alt="wechatMiniprogram" src="https://img.alicdn.com/tfs/TB1slcYdxv1gK0jSZFFXXb0sXXa-200-200.svg" width="25px" height="25px" title="微信小程序" /> <img alt="bytedanceMicroApp" src="https://gw.alicdn.com/tfs/TB1jFtVzO_1gK0jSZFqXXcpaXXa-200-200.svg" width="25px" height="25px" title="字节跳动小程序" /> <img alt="kuaiShouMiniProgram" src="https://gw.alicdn.com/imgextra/i4/O1CN01kzmJMM24jcFEzp5Wv_!!6000000007427-2-tps-200-200.png" width="25px" height="25px" title="快手小程序" /> |
+| options.complete | `Function`  | The callback function used when the API call completed (always executed whether the call succeeds or fails) | No | - | <img alt="wechatMiniprogram" src="https://img.alicdn.com/tfs/TB1slcYdxv1gK0jSZFFXXb0sXXa-200-200.svg" width="25px" height="25px" title="微信小程序" /> <img alt="bytedanceMicroApp" src="https://gw.alicdn.com/tfs/TB1jFtVzO_1gK0jSZFqXXcpaXXa-200-200.svg" width="25px" height="25px" title="字节跳动小程序" /> <img alt="kuaiShouMiniProgram" src="https://gw.alicdn.com/imgextra/i4/O1CN01kzmJMM24jcFEzp5Wv_!!6000000007427-2-tps-200-200.png" width="25px" height="25px" title="快手小程序" /> |
+
+
