e916aef45e1409a9cb9f91b7ace2fae166b45d1a
yitan.lgh <yitan.lgh@alibaba-inc.com>
2021-11-26 14:22:13 +0800

    feat: alipay support more authType

--- src/packages/open/authorize/src/ali-miniapp/index.ts
+++ src/packages/open/authorize/src/ali-miniapp/index.ts
@@ -6,10 +6,30 @@ import { CONTAINER_NAME } from '@utils/constant';
 
 function authorize(options: Options) {
   const { scope, success, fail, complete } = options;
+  const authTypeMap = {
+    'scope.record': 'MICROPHONE',
+    'scope.camera': 'CAMERA',
+    'scope.writePhotosAlbum': 'PHOTO',
+  };
   const scopeMap = {
+    'scope.userInfo': 'auth_user',
     auth_base: 'auth_base',
     auth_zhima: 'auth_zhima',
   };
+  if (authTypeMap[scope]) {
+    return my.showAuthGuide({
+      authType: authTypeMap[scope],
+      success(res?) {
+        success && success(res);
+      },
+      fail(res) {
+        fail && fail(res);
+      },
+      complete(res) {
+        complete && complete(res);
+      },
+    });
+  }
   return my.getAuthCode({
     scopes: scopeMap[scope] ? scopeMap[scope] : 'auth_user',
     success(res?) {
