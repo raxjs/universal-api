3125b68144f42f518386df44abda42d243455aa8
chenrongyan.cry <chenrongyan.cry@alibaba-inc.com>
2022-02-23 17:16:52 +0800

    fix: add ua condition

--- src/packages/base/env/src/index.ts
+++ src/packages/base/env/src/index.ts
@@ -9,6 +9,10 @@ declare const global: any;
 declare const process: any;
 declare const pha: any;
 declare const WindVane: any;
+declare const navigator: {
+  userAgent?: string;
+  swuserAgent?: string;
+};
 
 export const isWeb = typeof window !== 'undefined' && 'onload' in window;
 export const isNode = typeof process !== 'undefined' && !!(process.versions && process.versions.node);
@@ -25,8 +29,12 @@ export const isKuaiShouMiniProgram = typeof ks !== 'undefined' && ks !== null &&
 export const isWeChatMiniProgram = !isByteDanceMicroApp && typeof wx !== 'undefined' && wx !== null && (typeof wx.request !== 'undefined' || typeof wx.miniProgram !== 'undefined');
 export const isQuickApp = typeof global !== 'undefined' && global !== null && typeof global.callNative !== 'undefined' && !isWeex;
 export const isPHA = isWeb && typeof pha === 'object';
-export const isWindVane = isWeb && typeof WindVane !== 'undefined' && typeof WindVane.call !== 'undefined';
 export const isFRM = isMiniApp && isWeb && my.isFRM;
+
+// WindVane.call is a function while page importing windvane.js
+const ua = typeof navigator === 'object' ? (navigator.userAgent || navigator.swuserAgent) : '';
+export const isWindVane = /.+AliApp\((\w+)\/((?:\d+\.)+\d+)\).* .*(WindVane)(?:\/((?:\d+\.)+\d+))?.*/.test(ua)
+  && isWeb && typeof WindVane !== 'undefined' && typeof WindVane.call !== 'undefined';
 
 export default {
   isWeb,
