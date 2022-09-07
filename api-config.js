5e7ffe6018830d15dcf55c439ee08700c9e69162
yitan.lgh <yitan.lgh@alibaba-inc.com>
2022-09-07 16:39:24 +0800

    fix: 更新changelog

--- api-config.js
+++ api-config.js
@@ -75,7 +75,7 @@ module.exports = {
     path: 'src/packages/device/clipboard/src/index.ts',
     pkgInfo: [
       {
-        version: '1.0.8',
+        version: '1.0.9',
         name: '@uni/clipboard',
       },
     ],
