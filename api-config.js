9f332d896a999a1baf7537a788c64fe93a3fb5ee
daijianjiang <875013491@qq.com>
2022-12-27 17:31:26 +0800

    fix: 修改了changelog和版本号

--- api-config.js
+++ api-config.js
@@ -46,7 +46,7 @@ module.exports = {
         name: '@uni/unit-tool',
         exports: '',
         dependencies: {
-          '@uni/system-info': '^1.0.8',
+          '@uni/system-info': '^1.0.10',
         },
       },
     ],
@@ -84,7 +84,7 @@ module.exports = {
     path: 'src/packages/device/systemInfo/src/index.ts',
     pkgInfo: [
       {
-        version: '1.0.9',
+        version: '1.0.10',
         name: '@uni/system-info',
       },
     ],
